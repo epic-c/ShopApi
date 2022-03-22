b42de676f74df3c361746d50b3bfe3d3725213da
yongyu.chen <yy.chen@getbrightauto.com>
2022-03-22 18:13:12 +0800

    Add project files.

---
+++ ShopApi/Controllers/ShopController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopApi.Models;
+
+namespace ShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Commodity>>> GetCommoditys()
+        {
+            try
+            {
+                var imageArray = await System.IO.File.ReadAllBytesAsync(@"./DB/1.jpg");
+                var base64ImageRepresentation = Convert.ToBase64String(imageArray);
+
+                return Ok(new List<Commodity>()
+                {
+                    new()
+                    {
+                        Images = new []{base64ImageRepresentation},
+                        Comment = "",
+                        Name = "",
+                        Price = 0.0
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{Name}")]
+        public async Task<ActionResult<Commodity>> GetCommodity(string Name)
+        {
+            try
+            {
+                var imageArray = await System.IO.File.ReadAllBytesAsync(@"./DB/1.jpg");
+                var base64ImageRepresentation = Convert.ToBase64String(imageArray);
+
+                return Ok(new Commodity()
+                    {
+                        Images = new []{base64ImageRepresentation},
+                        Comment = "",
+                        Name = "",
+                        Price = 0.0
+                    });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ ShopApi/Models/Commodity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApi.Models
+{
+    public class Commodity
+    {
+        public string[] Images { get; set; }    //Base64
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Comment { get; set; }
+    }
+}
