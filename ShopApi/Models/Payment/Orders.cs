8c25355433e3c93347ef668d0fc48d9b9376029b
yongyu.chen <yy.chen@getbrightauto.com>
2022-05-21 21:17:34 +0800

    update

--- ShopApi/Models/Payment/Orders.cs
+++ ShopApi/Models/Payment/Orders.cs
@@ -5,5 +5,9 @@ namespace ShopApi.Models.Payment
     public class Orders
     {
         public List<Order> Value { get; set; }
+        //TODO 下面欄位先做接收，收完就把東西丟掉
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
     }
 }
